711e26a28a832ec71d2d9f7e0b8095c1cdf72656
Fabian Kongelf <fkongelf@gmail.com>
2023-02-02 17:23:18 +0100

    a

--- config-setup/beluga/beluga-launch.sh
+++ config-setup/beluga/beluga-launch.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
 
 # This file was curated by Vortex - DevOps
-# If any questions contac vortex devops
+# If any questions contact vortex devops
 
-# source files and declere global variable
+# source files and decleare global variable
 # -----------------------------------------------------------------------------------------------------------
+
 source progress.sh # progress bar
+
+echo -ne "beluga-launch v0.0.1\n\n"
 
 # init enviorment
 # -----------------------------------------------------------------------------------------------------------
 
-echo -ne "\n!## Initialize environment ##!\n\n"
-
+echo -ne "\033[1;30m"
+# echo -ne "Initialize environment...\n"
 apt-get update -qq | echo -ne "Updating apt\n"
 apt-get upgrade -qq | echo -ne "Upgrading apt\n"
 apt-get install -yqq openssh-server | echo -ne "Installing ssh\n"
 apt-get install -yqq bc | echo -ne "Installing bc\n"
+echo -ne "\033[0m"
 
 # launch beluga
 # -----------------------------------------------------------------------------------------------------------
 
-echo -ne "\n!## launching beluga ##!\n\n"
+echo -ne "\nlaunching beluga \n"
 
 tasks=(
     "handshake",
@@ -32,25 +36,30 @@ tasks=(
     "suport scripts",
 )
 
+echo -ne "\n"
 
 tasks_in_total=37
 for current_task in $(seq $tasks_in_total) # simulates progress... 
     do
+    echo -ne "\e[1A"
     sleep 0.2 #simulate the task running
     msg=$(echo "msg of some kind")
     error="false"
-
-    if [ $current_task -eq 60 ]; then
-        msg=$(echo "problem with task ${current_task}")
+    echo -ne "\r[${current_task}] long and descriptive log message, which hopfuly are longer then the progress bar.       \n \033[2K \n"
+    
+    if [ $current_task -eq 40 ]; then
+        msg=$(printf "problem with task ${current_task}")
         error="true"
         show_progress $current_task $tasks_in_total $error $msg
         break
     else
         show_progress $current_task $tasks_in_total $error $msg
     fi
+    
 done
 
 printf "\n"
+echo -ne "Beluga ready\n"
 
 # search network
 # create handshacks with rpi and xavier
