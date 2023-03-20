1076074930f26fab0a5428dd90d626d3607fdb75
Fabian Kongelf <97341614+FabianKongelf@users.noreply.github.com>
2023-03-20 18:13:57 +0100

    Update bash.py

--- scripts/beluga/autoFAT/scripts/bash.py
+++ scripts/beluga/autoFAT/scripts/bash.py
@@ -2,6 +2,7 @@
 
 import subprocess
 
+# run command
 def run(command: str) -> bool:
     # runs command
     result = subprocess.run(command, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
@@ -14,6 +15,7 @@ def run(command: str) -> bool:
         # environment dont have pkg
         return False
 
+# add ssh wraping to command
 def ssh_run(ip: str, user: str, pwd: str, command: str) -> bool:
     # sshpass applies the password to an ssh command
     # ssh connect you to another device
@@ -22,6 +24,7 @@ def ssh_run(ip: str, user: str, pwd: str, command: str) -> bool:
     
     return run(ssh_command)
 
+# check if apt package exist on craptop
 def hasDPKG(pkg: str) -> bool:
     # bash command to run ("> /dev/null 2>&1" removes output)
     command = 'dpkg -s ' + pkg + ' > /dev/null 2>&1'
@@ -32,6 +35,7 @@ def hasDPKG(pkg: str) -> bool:
         print("\033[0;31m", "Error: ", "\033[0m", "Missing package: ", pkg, ", try: apt-get install ", pkg, sep="")
         return False
 
+# ping device
 def ping(ip: str) -> bool:
     command = 'ping -c 4 ' + ip + ' | grep "4 received" > /dev/null 2>&1'
 
@@ -41,6 +45,17 @@ def ping(ip: str) -> bool:
         print("\033[0;31m", "Error: ", "\033[0m", "Unable to ping/reach ", ip, sep="")
         return False
 
+# test ssh by try to run ls though ssh
+def ssh_ls(device: object) -> bool:
+    command = 'ls > /dev/null 2>&1'
+    
+    if ssh_run(device["ip_address"], device["user"], device["password"], command):
+        return True
+    else:
+        print("\033[0;31m", "Error: ", "\033[0m", "Unable to ssh into: ", device["name"], sep="")
+        return False
+
+# validate env var on devices though ssh
 def valEnv(device: object, variable: str, value: str) -> bool:
     command = 'printenv '+ variable + ' | grep "' + value + '" > /dev/null 2>&1'
 
