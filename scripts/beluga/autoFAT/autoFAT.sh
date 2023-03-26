c04bd12b5d73bfb24569936d119db9bec014dd7a
Nils Loe <nils.loe@outlook.com>
2023-03-26 14:44:37 +0200

    Modules installed validation

--- scripts/beluga/autoFAT/autoFAT.sh
+++ scripts/beluga/autoFAT/autoFAT.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 
-python3 main.py
+# List of required packages
+required_packages=("rich" "pyyaml")
+
+# Flag to keep track of package installation status
+packages_installed=true
+
+# Check if each required package is installed
+for package in "${required_packages[@]}"
+do
+    if ! python -c "import $package" &> /dev/null; then
+        echo "$package is not installed"
+        packages_installed=false
+    fi
+done
+
+# If all packages are installed, run the main.py script
+if [ "$packages_installed" = true ]; then
+    python main.py
+else
+    echo "Cannot run main.py because packages are missing"
+fi
