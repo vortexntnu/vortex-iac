2ee5281cdc1d07ca25ab836cfcd60326ee5975bf
Fabian Kongelf <fkongelf@gmail.com>
2023-03-28 20:01:53 +0200

    w

--- scripts/beluga/autoFAT/autoFAT.sh
+++ scripts/beluga/autoFAT/autoFAT.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 # List of required packages
-required_packages=("rich" "pyyaml")
+required_packages=("rich" "PyYAML")
 
 # Flag to keep track of package installation status
-packages_installed=true
+packages_installed="true"
 
 # Check if each required package is installed
-for package in "${required_packages[@]}"
+for package in ${required_packages[@]}
 do
-    if ! python -c "import $package" &> /dev/null; then
-        echo "$package is not installed"
-        packages_installed=false
+    if ! pip list | grep $package >/dev/null; then
+        echo -ne "\033[0;33mWarning\033[0m: Python / pip module: $package is not installed, try pip install $package\n"
+        packages_installed="false"
     fi
 done
 
 # If all packages are installed, run the main.py script
-if [ "$packages_installed" = true ]; then
+if [ $packages_installed == "true" ]; then
     python main.py
 else
-    echo "Cannot run main.py because packages are missing"
-fi
+    echo -ne "\033[0;31mAbort\033[0m: did not launch autoFAT as python module is missing\n"
+fi  
